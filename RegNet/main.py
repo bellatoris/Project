09b5e78e4fe72f90d039d735849dfb1faa7ca317
doogie <bellatoris@snu.ac.kr>
2021-07-15 18:04:08 +0900

    add ssim

--- RegNet/main.py
+++ RegNet/main.py
@@ -15,6 +15,7 @@ from imageio import imwrite
 
 
 dataset_dir = '../datasets'
+dataIndex = [15,36,65,80,85,87,108,118,148,149,156]
 
 
 def parsing_minibatch(input1, batch_size):
@@ -26,15 +27,18 @@ def parsing_minibatch(input1, batch_size):
     return input1
 
 
-def validateModel_simple(encoder_decoder, iteration, use_l2loss=False):
-    if use_l2loss:
+def validateModel_simple(encoder_decoder, iteration, loss_mode):
+    if loss_mode == 'l1':
         outDir = './validation_l2/'
+    elif loss_mode == 'l1ssim':
+        outDir = './validation_l1ssim/'
     else:
-        outDir = './validation/'
+        outDir = './validation_l1/'
+
     os.makedirs(outDir, exist_ok=True)
 
     for iterVal in range(5):
-        output_tmp = miniBatch_generate(dataset_dir, 1, False, dataIndex=[14])
+        output_tmp = miniBatch_generate(dataset_dir, 1, False, dataIndex=dataIndex)
         gtPath = outDir + 'depth_gt_' + str(iterVal) + '_' + str(iteration) + '.png'
         gtImagePath = outDir + 'image_gt_' + str(iterVal) + '_' + str(iteration) + '.png'
         gtImage2Path = outDir + 'image2_gt_' + str(iterVal) + '_' + str(iteration) + '.png'
@@ -59,14 +63,18 @@ def validateModel_simple(encoder_decoder, iteration, use_l2loss=False):
 
 
 def main():
-    use_l2loss = sys.argv[1] == 'true'
+    loss_mode = sys.argv[1]
 
-    if use_l2loss:
+    if loss_mode == 'l1':
         outDir = './model_l2/'
+        gpu = 0
+    elif loss_mode == 'l1ssim':
+        outDir = './model_l1ssim/'
         gpu = 1
     else:
-        outDir = './model/'
-        gpu = 0
+        outDir = './model_l1/'
+        gpu = 2
+
     os.makedirs(outDir, exist_ok=True)
 
     torch.cuda.set_device(gpu) # change allocation of current GPU
@@ -89,6 +97,7 @@ def main():
 
     parameters_to_train = list(encoder_decoder.parameters())
     optimizer = torch.optim.Adam(parameters_to_train, lr=lr, weight_decay=2e-4)
+    ssim = SSIM()
 
     # Call SUN3D directories
     batch_size = 64
@@ -100,13 +109,13 @@ def main():
 
         t = time.time()
 
-        minibatch_tmp = miniBatch_generate(dataset_dir, batch_size, dataIndex=[14])
+        minibatch_tmp = miniBatch_generate(dataset_dir, batch_size, dataIndex=dataIndex)
         input = parsing_minibatch(minibatch_tmp, batch_size)
 
         if (i % 10000 == 0):
             optimizer.lr = 1e-5
 
-        (loss, depth_loss, pose_loss) = train(input, encoder_decoder, optimizer, use_l2loss)
+        (loss, depth_loss, pose_loss) = train(input, encoder_decoder, optimizer, ssim, loss_mode)
 
         if i % 10 == 0:
             elapse_t = time.time() - t
@@ -122,10 +131,10 @@ def main():
 
         if i % 100 == 0:
             torch.save({'state_dict': encoder_decoder.state_dict()}, os.path.join(outDir, 'encdec_{0}.pth'.format(i)))
-            validateModel_simple(encoder_decoder, i, use_l2loss)
+            validateModel_simple(encoder_decoder, i, loss_mode)
 
 
-def train(input, encoder_decoder, optimizer, use_l2loss=False):
+def train(input, encoder_decoder, optimizer, ssim, loss_mode):
     image1 = torch.autograd.Variable(input['input_image_first'].cuda())
     image2 = torch.autograd.Variable(input['input_image_second'].cuda())
     egomotion = torch.autograd.Variable(input['target_egomotion'].cuda())
@@ -133,7 +142,7 @@ def train(input, encoder_decoder, optimizer, use_l2loss=False):
 
     image_pair = torch.cat((image1, image2), dim=1)
 
-    if use_l2loss:
+    if loss_mode == 'l2':
         loss_function = torch.nn.MSELoss()
     else:
         loss_function = torch.nn.L1Loss()
@@ -144,7 +153,11 @@ def train(input, encoder_decoder, optimizer, use_l2loss=False):
     depth_loss = loss_function(depth_output, depth)
     pose_loss = loss_function(pose_output, egomotion)
 
-    loss = depth_loss + pose_loss
+    if loss_mode == 'l2' or loss_mode == 'l1':
+        loss = depth_loss + pose_loss
+    else:
+        ssim_loss = ssim(depth_output, depth).mean()
+        loss = ssim_loss + depth_loss + pose_loss
 
     optimizer.zero_grad()
     loss.backward()
